015c07026b06bae90d151e858c5525876d3c5e20
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-03-09 05:33:28 +0530

    Add files via upload

---
+++ shivu/config.py
@@ -0,0 +1,24 @@
+class Config(object):
+    LOGGER = True
+
+    # Get this value from my.telegram.org/apps
+    OWNER_ID = "2064735436"
+    SUDOERS = "2064735436", "6295947116"
+    GROUP_ID = -1002041217401
+    TOKEN = "7198935197:AAFh6XR9ZIk4OWA0rZHfptM_eLh4QaY69yA"
+    mongo_url = "mongodb+srv://gotouhitoriii:7vzXrCihdSZfTtEs@haremgod.rxndc2y.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url1.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url2.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url3.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url4.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url5.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url6.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url7.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url8.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url9.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url10.png", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url11.png"]
+    SUPPORT_CHAT = "CHATHV3N"
+    UPDATE_CHAT = "LOGSHV3N"
+    BOT_USERNAME = "HaremGodBot"
+    CHARA_CHANNEL_ID = "-1001782672183"
+    api_id = 21927988
+    api_hash = "e18f720acdff1e5b0ec80616aecd8a5a"
+
+    
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
