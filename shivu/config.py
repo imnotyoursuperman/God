e95522965c2696d74b4f0237a2242f634fde83e5
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-07-30 18:13:25 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,12 +3,12 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "2064735436"
-    SUDOERS = "6295947116"
+    SUDOERS = "6785172469"
     GROUP_ID = -1001440080728
-    TOKEN = "7198935197:AAGEFeXUpNunaKUTT1llUZDt7zXc3VsMObI"
+    TOKEN = "7198935197:AAGjfjzC9zHi_c1aN4jlqDKLea8CU48u48A"
     mongo_url = "mongodb+srv://gotouhitoriii:7vzXrCihdSZfTtEs@haremgod.rxndc2y.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url1.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url2.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url3.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url4.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url5.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url6.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url7.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url8.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url9.jpg", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url10.png", "https://raw.githubusercontent.com/theredactedentity/databasepics/main/photo_url11.png"]
-    SUPPORT_CHAT = "GlobalAnimeCommunityOfficial"
+    SUPPORT_CHAT = "NineProvincesNumberOneGroup"
     UPDATE_CHAT = "BotsByChad"
     BOT_USERNAME = "HaremGodBot"
     CHARA_CHANNEL_ID = "-1001782672183"
